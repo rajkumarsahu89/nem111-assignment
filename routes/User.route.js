1bb63ecb81a6265ea4df8b44ea5fbccc3c1619d5
rajkumarsahu89 <sahukumarraj89@gmail.com>
2023-02-19 23:18:57 +0530

    add

---
+++ routes/User.route.js
@@ -0,0 +1,92 @@
+const bcrypt=require("bcrypt")
+
+const jwt=require("jsonwebtoken");
+
+require("dotenv").config();
+
+const {UserModel}=require("../models/User.model")
+
+const express=require("express");
+
+const userRouter=express.Router();
+
+userRouter.use(express.json());    
+
+
+userRouter.post("/register",async (req,res)=>{
+    const {name,email,pass,age}=req.body;
+    try{
+        bcrypt.hash(pass, 5, async(err,secure_password)=>{
+            try{
+                const user=new UserModel({name,email,pass:secure_password,age});
+                await user.save();
+                res.send("Registered");
+            }catch(err){
+                console.log(err)
+            }
+        });
+    }catch(err){
+        res.send("Something went wrong");
+        console.log(err)
+    }
+})
+
+userRouter.post("/login",async (req,res)=>{
+    const {email,pass}=req.body;
+    try{
+        const user=await UserModel.find({email});
+        const hashed_pass=user[0].pass
+        if(user.length>0){
+            bcrypt.compare(pass, hashed_pass,(err,result)=>{
+                if(result){
+                    var token = jwt.sign({ userID:user[0]._id }, process.env.key);
+                    res.send({"msg":"Login Success","token":token});
+
+                }else{
+                    res.send("Wrong Credentials");
+                    console.log(err);
+                }
+            });
+        }
+        // console.log(user)
+    }catch(err){
+        res.send("Something went wrong");
+        console.log(err)
+    }
+})
+
+userRouter.get("/about",(req,res)=>{
+    res.send("About Page")
+})
+
+userRouter.get("/contact",(req,res)=>{
+    
+    res.send("Contact Page")
+})
+
+userRouter.get("/data",(req,res)=>{
+    const token=req.headers.authorization;
+    // console.log(token)
+    jwt.verify(token, process.env.key, (err,decode)=>{
+        if(err){
+            res.send("Invalid Token")
+        }else{
+            res.send("Data Page")
+        }
+    })
+})
+
+userRouter.get("/cart",(req,res)=>{
+    const token=req.headers.authorization;
+    // console.log(token)
+    jwt.verify(token, process.env.key, (err,decode)=>{
+        if(err){
+            res.send("Invalid Token")
+        }else{
+            
+            res.send("Cart Page")
+        }
+    })
+})
+
+module.exports={userRouter};
